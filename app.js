974bb0119d018a53962e514c541d0c9ce61b31f0
Liberatone <liberatone@sung-mbpr15i9.lan>
2018-11-04 01:55:12 -0400

    change3

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var foodArray = ["pizza", "pho", "ramen", "pasta", "salad"];
         var food = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=y6VwG67BXWfJhwEOrpnQAGpSaQMVsUuf&limit=10";
         console.log(queryURL);
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
 
                 var results = response.data;
 
@@ -23,8 +23,10 @@ var foodArray = ["pizza", "pho", "ramen", "pasta", "salad"];
                     var dataTitle = results[i].title;
                     var dataRating = results[i].rating;
                     
+
+                    var masterDiv = $("<div class='row border border-danger p-3 mb-4 bg-white rounded'>");
                     // Creating div that'll contain all gif and related data
-                    var gifDiv = $("<div class='shadow-lg p-3 mb-4 bg-white rounded'>");
+                    var gifDiv = $("<div class='col-sm-7'>");
                     // Creating and storing the image tag
                     var foodGif = $("<img>");
                     // Setting attribute to foodGif
@@ -36,33 +38,44 @@ var foodArray = ["pizza", "pho", "ramen", "pasta", "salad"];
                     // Storing title and rating data in <p>
                     var pTitle = $("<p>").text("Title: " + dataTitle);
                     var pRating = $("<p>").text("Rating: " + dataRating);
-    
+
+                    // just for fun let's yelp it
+                    var yelpDiv = $("<div class='col-sm-5'>");
+                    var yelpUrl = "https://www.yelp.com/search?find_desc=" + food + "&find_loc=New+York%2C+NY&ns=1";
+                    var pYelp1 = $("<p>").text("Search for the best " + food + " restaurant in New York City");
+                    var pYelp2 = $("<a>");
+                    pYelp2.addClass("btn btn-danger m-2 mb-5");
+                    pYelp2.text("Yelp!");
+                    pYelp2.attr("href", yelpUrl);
                     // Append 
                     gifDiv.append(pTitle);
                     gifDiv.append(pRating);
                     gifDiv.append(foodGif);
+                    yelpDiv.append(pYelp1);
+                    yelpDiv.append(pYelp2);
+                    masterDiv.append(gifDiv);
+                    masterDiv.append(yelpDiv);
+
                     // Prepend
-                    $("#giphy-area").prepend(gifDiv);
+                    $("#giphy-area").prepend(masterDiv);
 
-                    $("#gif-id").on("click", function() {
-                        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-                        var state = $(this).attr("data-state");
-                        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                        // Then, set the image's data-state to animate
-                        // Else set src to the data-still value
-                        if (state === "still") {
-                          $(this).attr("src", $(this).attr("data-animate"));
-                          $(this).attr("data-state", "animate");
-                        } else {
-                          $(this).attr("src", $(this).attr("data-still"));
-                          $(this).attr("data-state", "still");
-                        }
-                      });
-
+                        $("#gif-id").on("click", function() {
+                            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                            var state = $(this).attr("data-state");
+                            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+                            // Then, set the image's data-state to animate
+                            // Else set src to the data-still value
+                            if (state === "still") {
+                            $(this).attr("src", $(this).attr("data-animate"));
+                            $(this).attr("data-state", "animate");
+                            } else {
+                            $(this).attr("src", $(this).attr("data-still"));
+                            $(this).attr("data-state", "still");
+                            }
+                        });
                 }
-
             });
-        };
+    };
 
 
     function renderButtons() {
@@ -75,7 +88,7 @@ var foodArray = ["pizza", "pho", "ramen", "pasta", "salad"];
             // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var a = $("<button>");
             // Adding a class of btn btn-outline-danger m-2 to our button
-            a.addClass("btn btn-outline-danger m-2");
+            a.addClass("btn btn-outline-danger m-2 mb-5");
             a.attr("id", "button-id");
             // Adding a data-attribute
             a.attr("data-name", foodArray[i]);
